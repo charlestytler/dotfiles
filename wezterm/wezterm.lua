1968be9667d3e9b60a945c55a001c8dbc5c1f73a
Charlie Tytler <cjtytler@gmail.com>
2024-12-12 22:22:17 -0800

    wezterm - config formatting

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -1,13 +1,13 @@
-local wezterm = require 'wezterm'
+local wezterm = require("wezterm")
 local config = wezterm.config_builder()
 
-config.font = wezterm.font 'Hack Nerd Font'
+config.font = wezterm.font("Hack Nerd Font")
 --config.font = wezterm.font 'Iosevka Nerd Font'
 config.font_size = 14
 
---config.color_scheme = 'Palenight (Gogh)'
---config.color_scheme = 'Ayu Mirage'
-config.color_scheme = 'Decaf (base16)'
+-- config.color_scheme = "Palenight (Gogh)"
+-- config.color_scheme = "Ayu Mirage"
+config.color_scheme = "Decaf (base16)"
 
 config.window_background_opacity = 0.92
 config.macos_window_background_blur = 10
@@ -15,6 +15,6 @@ config.macos_window_background_blur = 10
 config.enable_tab_bar = false
 config.window_decorations = "TITLE | RESIZE"
 
-config.window_close_confirmation = 'NeverPrompt'
+config.window_close_confirmation = "NeverPrompt"
 
 return config
