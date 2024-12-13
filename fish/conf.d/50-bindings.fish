268bb291083f4ea4bb0e7ffb27a968a22d84073d
Charlie Tytler <cjtytler@gmail.com>
2024-12-12 22:19:34 -0800

    fish - add nvim sessions and edit cmd in nvim

--- fish/conf.d/50-bindings.fish
+++ fish/conf.d/50-bindings.fish
@@ -5,6 +5,7 @@ function fish_user_key_bindings
       # This binds Ctrl-Space to accept a fish autocomplete suggestion (same as right-arrow).
     bind -M $mode -k nul forward-char
     bind -M $mode \cp open_files_in_nvim
+    bind -M $mode \ce edit_command_buffer
   end
 
 end
