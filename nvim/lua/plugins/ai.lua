366136b7ef20b1c3e6a9d519dcc572539f15d11c
Charlie Tytler <cjtytler@gmail.com>
2025-01-06 14:45:08 -0800

    Switch codeium to copilot

---
+++ nvim/lua/plugins/ai.lua
@@ -0,0 +1,55 @@
+return {
+  {
+    "github/copilot.vim",
+    cmds = {
+      "Copilot",
+    },
+    opts = {},
+  },
+  {
+    "olimorris/codecompanion.nvim",
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+      "nvim-treesitter/nvim-treesitter",
+    },
+    event = "VeryLazy",
+    config = function() -- This is the function that runs, AFTER loading
+      require("codecompanion").setup {
+        strategies = {
+          chat = {
+            adapter = "copilot",
+          },
+          inline = {
+            adapter = "copilot",
+          },
+        },
+      }
+    end,
+    keys = {
+      { "<leader>a", "<cmd>CodeCompanionChat<CR>", desc = "AI chat" },
+    },
+  },
+}
+
+-- return {
+--   "Exafunction/codeium.nvim",
+--   event = "BufEnter",
+--   dependencies = {
+--     "nvim-lua/plenary.nvim",
+--     "hrsh7th/nvim-cmp",
+--   },
+--   keys = {
+--     { "<leader>a", "<cmd>Codeium Chat<CR>", desc = "Ask Codeium chat" },
+--   },
+--   opts = {
+--     enable_cmp_source = true,
+--     virtual_text = {
+--       enabled = true,
+--       key_bindings = {
+--         accept = "<Tab>",
+--         accept_word = ";<Tab>",
+--         clear = "jl",
+--       },
+--     },
+--   },
+-- }
