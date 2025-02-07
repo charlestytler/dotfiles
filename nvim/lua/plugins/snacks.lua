3f65881532717d6ad5f253be13f914862d28d656
Charlie Tytler <cjtytler@gmail.com>
2025-02-06 23:49:35 -0800

    nvim: Make custom snacks pickers more generic

--- nvim/lua/plugins/snacks.lua
+++ nvim/lua/plugins/snacks.lua
@@ -1,16 +1,14 @@
-local custom_pickers = {}
-
-function custom_pickers.git(picker_opts)
+-- See https://github.com/folke/snacks.nvim/discussions/702 for examples
+local function pick_cmd_result(picker_opts)
+  local git_root = Snacks.git.get_root()
   local function finder(opts, ctx)
-    local cwd = Snacks.git.get_root()
     return require("snacks.picker.source.proc").proc({
       opts,
       {
-        cmd = "git",
+        cmd = picker_opts.cmd,
         args = picker_opts.args,
-        -- @param item snacks.picker.finder.Item
         transform = function(item)
-          item.cwd = cwd
+          item.cwd = picker_opts.cwd or git_root
           item.file = item.text
         end,
       },
@@ -25,8 +23,12 @@ function custom_pickers.git(picker_opts)
   }
 end
 
+-- Custom Pickers
+local custom_pickers = {}
+
 function custom_pickers.git_show()
-  custom_pickers.git {
+  pick_cmd_result {
+    cmd = "git",
     args = { "diff-tree", "--no-commit-id", "--name-only", "--diff-filter=d", "HEAD", "-r" },
     name = "git_show",
     title = "Git Last Commit",
@@ -35,7 +37,8 @@ function custom_pickers.git_show()
 end
 
 function custom_pickers.git_diff_upstream()
-  custom_pickers.git {
+  pick_cmd_result {
+    cmd = "git",
     args = { "diff-tree", "--no-commit-id", "--name-only", "--diff-filter=d", "HEAD@{u}..HEAD", "-r" },
     name = "git_diff_upstream",
     title = "Git Branch Changed Files",
