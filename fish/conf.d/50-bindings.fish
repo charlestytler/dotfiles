ffed883731518e550ffca98cd2f00b89bd58cf36
Charles Tytler <cjtytler@gmail.com>
2026-02-03 11:45:58 -0800

    fish: revert bindings change (upgrading to fish v4 was the correct fix)

--- fish/conf.d/50-bindings.fish
+++ fish/conf.d/50-bindings.fish
@@ -6,11 +6,6 @@ function fish_user_key_bindings
     bind -M $mode ctrl-space accept-autosuggestion
     bind -M $mode ctrl-p open_files_in_nvim
     bind -M $mode ctrl-e edit_command_buffer
-
-    # Alternate binding method, for terminals that read Ctrl+Space as nul.
-    bind -M $mode --key nul accept-autosuggestion
-    bind -M $mode \cp open_files_in_nvim
-    bind -M $mode \ce edit_command_buffer
   end
 
 end
