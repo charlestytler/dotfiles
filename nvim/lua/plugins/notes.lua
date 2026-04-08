c0ef650b707952e7b2f3718147229bf6c8abf5e5
Charlie Tytler <cjtytler@gmail.com>
2026-04-08 08:33:33 -0700

    Add Obsidian

--- nvim/lua/plugins/notes.lua
+++ nvim/lua/plugins/notes.lua
@@ -86,6 +86,28 @@ local haunt = {
   end,
 }
 
+local obsidian = {
+  "obsidian-nvim/obsidian.nvim",
+  version = "*", -- use latest release, remove to use latest commit
+  ---@module 'obsidian'
+  ---@type obsidian.config
+  opts = {
+    legacy_commands = false, -- this will be removed in the next major release
+    workspaces = {
+      {
+        name = "notes",
+        path = "~/notes",
+      },
+      -- {
+      --   name = "personal",
+      --   path = "~/vaults/personal",
+      -- },
+    },
+    ui = { enable = false },
+  },
+}
+
 return {
   haunt,
+  obsidian,
 }
